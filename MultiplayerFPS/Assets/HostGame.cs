26f0e19ebf41f2b3b76faf7a029ad68dbfb2abc1
Asbjørn Thirslund <at@brackeys.com>
2016-07-14 15:25:40 +0200

    Video 18: Host Game

---
+++ MultiplayerFPS/Assets/HostGame.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+	[SerializeField]
+	private uint roomSize = 6;
+
+	private string roomName;
+
+	private NetworkManager networkManager;
+
+	void Start ()
+	{
+		networkManager = NetworkManager.singleton;
+		if (networkManager.matchMaker == null)
+		{
+			networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetRoomName (string _name)
+	{
+		roomName = _name;
+	}
+
+	public void CreateRoom ()
+	{
+		if (roomName != "" && roomName != null)
+		{
+			Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+			networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", networkManager.OnMatchCreate);
+		}
+	}
+
+}
